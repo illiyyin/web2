38978b963c41274d06810e4e83ae2c7dd9a33bc3
illiyyin <iilliyyin@gmail.com>
2018-11-12 09:06:12 +0700

    edit file

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -33,6 +33,43 @@ class FileController extends Controller
 		$data->save();
 		return redirect()->route('file.index')->with('alert-success','Berhasil Menambahkan Data!');
     }
+	
+	public function edit($id)
+    {
+        $data = ModelFile::where('id',$id)->first();
+		return view('fileedit',compact('data'));
+    }
+	
+	public function update(Request $request, $id)
+    {
+        $data = ModelFile::findOrFail($id);
+		$data->nama = $request->name;
+		if(empty($request->file('file'))){
+			$data->file=$data->file;
+		}
+		else{
+			unlink('uploads/file/'.$data->file);
+			$uploadedFile = $request->file('file');
+			$nama = date('Ydhmis').".".$uploadedFile->getClientOriginalExtension();
+			$uploadedFile->move('uploads/file',$nama);
+			$data->file = $nama;
+		}
+		$data->save();
+		return redirect()->route('file.index')->with('alert-success','Berhasil Menambahkan Data!');
+    }
+	public function show($id)
+    {
+        //
+    }
+	
+	public function destroy($id)
+    {
+        $data = ModelFile::findOrFail($id);
+        if($data->delete()){
+			unlink('uploads/file/'.$data->file);
+		}
+		return redirect()->route('file.index')->with('alert-success','Data berhasi dihapus!');
+    }
 
     /**
      * Show the form for creating a new resource.
---
+++ database/migrations/file.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Export to PHP Array plugin for PHPMyAdmin
+ * @version 0.2b
+ */
+
+//
+// Database `homestead`
+//
+
+// `homestead`.`file`
+$file = array(
+  array('id' => '1','nama' => 'qq','file' => '20181201114345.jpg','updated_at' => '2018-11-12 01:43:45','created_at' => '2018-11-12 01:03:37')
+);
